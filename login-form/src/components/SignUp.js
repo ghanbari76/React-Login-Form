b839e848ae129ef9c4eb1c918307f8d13799ccad
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-06-29 17:41:21 -0700

    Define the state and value of all inputs

--- login-form/src/components/SignUp.js
+++ login-form/src/components/SignUp.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 const SignUp = () => {
+    const [data,setData] = useState({
+        name : "",
+        email : "",
+        password : "",
+        confirmPassword : "",
+        isAccepted : false
+    })
     return (
         <div>
             <form>
                 <h2>SignUp</h2>
                 <div>
-                    <label>Name:</label>
-                    <input type='text' name='name' />
+                    <label>Name</label>
+                    <input type='text' name='name' value={data.name} />
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type='text' name='email' />
+                    <input type='text' name='email' value={data.email} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type='password' name='password' />
+                    <input type='password' name='password' value={data.password} />
                 </div>
                 <div>
                     <label>ConfirmPassword</label>
-                    <input type='password' name='confirmPassword' />
+                    <input type='password' name='confirmPassword' value={data.confirmPassword} />
                 </div>
                 <div>
                     <label>I accept terms of privacy policy</label>
-                    <input type='checkbox' name='isAccepted' />
+                    <input type='checkbox' name='isAccepted' value={data.isAccepted} />
                 </div>
                 <div>
                     <a href='#'>Login</a>

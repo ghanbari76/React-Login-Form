6861cd80a673ff43aa478235b45704ae69731884
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-06-29 17:27:57 -0700

    HTML template design

--- login-form/src/components/SignUp.js
+++ login-form/src/components/SignUp.js
@@ -3,7 +3,33 @@ import React from 'react';
 const SignUp = () => {
     return (
         <div>
-            
+            <form>
+                <h2>SignUp</h2>
+                <div>
+                    <label>Name:</label>
+                    <input type='text' name='name' />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type='text' name='email' />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type='password' name='password' />
+                </div>
+                <div>
+                    <label>ConfirmPassword</label>
+                    <input type='password' name='confirmPassword' />
+                </div>
+                <div>
+                    <label>I accept terms of privacy policy</label>
+                    <input type='checkbox' name='isAccepted' />
+                </div>
+                <div>
+                    <a href='#'>Login</a>
+                    <button>Sign Up</button>
+                </div>
+            </form>
         </div>
     );
 };
